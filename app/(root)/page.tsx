4282161bf8fc3860c3832a5ade2117df50f7a7ac
ISTIEAK AHMED <54891541+istieak-a@users.noreply.github.com>
2024-01-29 13:00:51 +0600

    Add new files and update styles

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function page() {
+  return (
+    <div className="m-10">
+      <Button size="lg" variant="ghost">Click me</Button>
+    </div>
+  );
+}
